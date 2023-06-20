010485093cac72952f589eec5de908a8a7edd86f
tom96da <129831775+tom96da@users.noreply.github.com>
2023-06-20 18:49:40 +0900

    update: make animation
    アニメーションを作成するプログラムを、配置表示とフラッディングの観察の2つに分割した。

--- src/makeFloodingGif.py
+++ src/makeFloodingGif.py
@@ -1,13 +1,12 @@
 '''
-   file: makePosGif.py
+   file: makeFloodingGif.py
    author: tom96da
-   brief: シミュレーションでのデバイスの動きをgifにする
+   brief: シミュレーションでのフラッディングの様子をgifにする
    date: 2023-05-17
 '''
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import matplotlib.patches as patches
 import pandas as pd
 
 plt.rcParams['font.family'] = 'sans-serif'
@@ -21,24 +20,20 @@ dev_y = []
 pos_x = []
 pos_y = []
 
-red = [3]
-cyan = [
-    [2, 9, 13, 19, 21],
-    [6, 11, 25, 36, 88],
-    [24, 29, 41, 42, 52, 67, 78, 81],
-    [23, 43, 50, 51, 54, 66, 80, 86, 94, 97],
-    [4, 5, 18, 20, 37, 53, 56, 57, 62, 73, 74, 83, 87, 92, 93, 98],
-    [0, 15, 16, 38, 61, 63, 65, 72, 76, 77, 82],
-    [8, 10, 14, 22, 28, 30, 35, 46, 55, 58, 60, 70, 71, 85, 89],
-    [7, 17, 39, 48, 49, 64, 69, 75, 79],
-    [27, 31, 33, 40, 44, 59, 68],
-    [34, 45, 84, 90, 91, 95],
-    [1, 47, 96, 99],
-    [12, 26, 32]
+source = [45]
+received = [
+    [72, 82, 86, 87, 90, 99],
+    [5, 8, 12, 14, 21, 22, 23, 29, 46, 58, 61, 62, 65, 67, 71, 88, 94, 97],
+    [0, 11, 13, 15, 17, 20, 31, 35, 36, 37, 38, 41, 47,
+        49, 50, 55, 56, 63, 74, 79, 81, 83, 92, 93],
+    [2, 3, 4, 9, 16, 24, 27, 39, 40, 51, 54, 66, 70, 75, 78, 91],
+    [10, 18, 19, 33, 34, 64, 73, 76, 77, 84, 85, 95],
+    [1, 6, 7, 25, 42, 44, 48, 53, 60, 80],
+    [28, 30, 43, 52, 57, 59, 68, 89, 96, 98],
+    [26, 32, 69],
 ]
-yellow = []
 
-num_step = len(cyan) + 1
+num_step = len(received) + 1
 
 for i in range(num_dev):
     fname.append("../tmp/dev_pos" + str(i) + ".csv")
@@ -75,16 +70,13 @@ def update(frame):
     ax.set_ylim(0, field_size)
 
     ax.plot(pos_x[move], pos_y[move], "o", c='k', ms=2)
-    for id in red:
+    for id in source:
         ax.plot(pos_x[move][id], pos_y[move][id], "o", c='r', ms=5)
-        # c = patches.Circle(
-        #     xy=(pos_x[move][id], pos_y[move][id]), radius=10, ec='k', fill=False)
-        # ax.add_patch(c)
+
     for step in range(steps):
-        for id in cyan[step]:
+        for id in received[step]:
             ax.plot(pos_x[move][id], pos_y[move][id], "o", c='c', ms=4)
-    # for id in yellow:
-    #     ax.plot(pos_x[move][id], pos_y[move][id], "o", c='g', ms=3)
+
     ax.plot(-1, -1, "o", c='r', ms=3, label='origin')
     ax.plot(-1, -1, "o", c='c', ms=3, label='reached data')
     plt.legend(fontsize=12, prop={"weight": "bold"},
@@ -93,5 +85,5 @@ def update(frame):
 
 anim = FuncAnimation(fig, update, frames=range(num_step), interval=600)
 
-# anim.save("anim.gif")
+anim.save("../tmp/fig/flooding.gif")
 plt.show()
---
+++ src/makePositonPlot.py
@@ -0,0 +1,82 @@
+'''
+   file: makePositonPlot.py
+   author: tom96da
+   brief: シミュレーションでのデバイスの動きと接続をgifにする
+   date: 2023-05-17
+'''
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.patches as patches
+import pandas as pd
+
+
+plt.rcParams['font.family'] = 'sans-serif'
+
+num_dev = 100
+num_frame = 0
+field_size = 60
+
+fname = []
+dev_x = []
+dev_y = []
+pos_x = []
+pos_y = []
+
+target = [45]
+paired = []
+connected = [11, 15, 49, 52, 62, 81]
+
+for i in range(num_dev):
+    fname.append("../tmp/dev_pos" + str(i) + ".csv")
+    data = pd.read_csv(fname[i])
+
+    dev_x.append(data['x'])
+    dev_y.append(data['y'])
+
+num_frame = len(dev_x[0])
+
+for i in range(num_frame):
+    frm_x = []
+    frm_y = []
+    for j in range(num_dev):
+        frm_x.append(dev_x[j][i])
+        frm_y.append(dev_y[j][i])
+    pos_x.append(frm_x)
+    pos_y.append(frm_y)
+
+fig = plt.figure(figsize=(5, 6))
+ax = fig.add_subplot(111)
+ax.set_aspect('equal')
+ax.tick_params(labelsize=12)
+
+
+def update(frame):
+    ax.cla()
+    ax.set_title("Position frame:" + str(frame), size=18, fontweight='bold')
+    ax.set_xlabel("x", size=18, fontweight='bold')
+    ax.set_ylabel("y", size=18, fontweight='bold')
+    ax.set_xlim(0, field_size)
+    ax.set_ylim(0, field_size)
+
+    ax.plot(pos_x[frame], pos_y[frame], "o", c='k', ms=2)
+    for id in target:
+        ax.plot(pos_x[frame][id], pos_y[frame][id], "o", c='r', ms=5)
+        c = patches.Circle(
+            xy=(pos_x[frame][id], pos_y[frame][id]), radius=10, ec='k', fill=False)
+        ax.add_patch(c)
+    for id in paired:
+        ax.plot(pos_x[frame][id], pos_y[frame][id], "o", c='y', ms=3)
+    for id in connected:
+        ax.plot(pos_x[frame][id], pos_y[frame][id], "o", c='g', ms=3)
+
+    ax.plot(-1, -1, "o", c='y', ms=3, label='paired')
+    ax.plot(-1, -1, "o", c='g', ms=3, label='connected')
+    plt.legend(fontsize=12, prop={"weight": "bold"},
+               loc="lower right", bbox_to_anchor=(1.05, -0.22))
+
+
+anim = FuncAnimation(fig, update, frames=range(num_frame), interval=200)
+
+anim.save("../tmp/fig/position.gif")
+plt.show()
