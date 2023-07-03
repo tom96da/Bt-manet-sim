6b7c85ed8ea78ac06b89b97b55f21a35ddabac22
tom96da <129831775+tom96da@users.noreply.github.com>
2023-07-03 21:50:34 +0900

    add: test code (frequency)
    度数分布を作成、集計する機能を試作した。

---
+++ test/ｆrequency.cpp
@@ -0,0 +1,48 @@
+/*!
+ * @file ｆrequency.cpp
+ * @author tom96da
+ * @brief 度数分布の作成と集計
+ * @date 2023-07-03
+ */
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+std::map<int, int> calculateFrequency(const std::vector<int>& data) {
+    std::map<int, int> frequencyMap;
+
+    // データの個数を数える
+    for (int num : data) {
+        frequencyMap[num]++;
+    }
+
+    return frequencyMap;
+}
+
+void printFrequency(const std::map<int, int>& frequencyMap) {
+    for (const auto& [v, n] : frequencyMap) {
+        std::cout << v << ": " << n << std::endl;
+    }
+}
+
+void mergeFrequency(std::map<int, int>& frequency_original,
+                    std::map<int, int> frequency_device) {
+    for (auto [num_hop, num_device] : frequency_device) {
+        frequency_original[num_hop] += num_device;
+    }
+};
+
+int main() {
+    std::map<int, int> sum;
+    std::vector<int> data = {1, 2, 3, 1, 2, 4, 5, 2, 3, 4, 1, 3};
+
+    std::map<int, int> frequencyMap = calculateFrequency(data);
+    mergeFrequency(sum, frequencyMap);
+    mergeFrequency(sum, frequencyMap);
+
+    printFrequency(frequencyMap);
+    printFrequency(sum);
+
+    return 0;
+}
